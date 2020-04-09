73d8d97734904370deab0afbe278d487977d917f
Victor Sibanda <victormsibanda1@gmail.com>
2020-04-09 09:19:37 +0100

    initial commit-working vagrant

---
+++ provision.sh
@@ -0,0 +1,13 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install python3.7 -y
+sudo apt install -y python3-pip
+
+sudo apt install build-essential libssl-dev libffi-dev python3-dev -y
+
+
+sudo pip3 install -r /home/ubuntu/code/requirements.txt
+sudo pip3 install bs4
+sudo mkdir /home/vagrant/Downloads
+sudo chmod 777 /home/vagrant/Downloads
