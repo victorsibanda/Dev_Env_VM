1e2b1da424096142754dede989e34c35e6447ba2
Victor Sibanda <victormsibanda1@gmail.com>
2020-04-16 15:31:51 +0100

    Edited Provision.sh

--- provision.sh
+++ provision.sh
@@ -1,13 +1,19 @@
+
+#Updates Sources List and Upgrades Apps
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
+#Install Python3 and pip
 sudo apt-get install python3.7 -y
 sudo apt install -y python3-pip
 
+## Install Build Essentials May be Needed
 sudo apt install build-essential libssl-dev libffi-dev python3-dev -y
 
-
+## Install all requirements including beautifulsound4 seperately
 sudo pip3 install -r /home/ubuntu/code/requirements.txt
 sudo pip3 install bs4
+
+#Make Directory Downloads and Give Permissions
 sudo mkdir /home/vagrant/Downloads
 sudo chmod 777 /home/vagrant/Downloads
